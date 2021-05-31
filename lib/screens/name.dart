2c842ea82fc45ba693f8c33b474bc79c89a575d9
Kako Botasso <kakodev@gmail.com>
2021-05-31 00:42:14 -0300

    Adding a Bloc to change name

---
+++ lib/screens/name.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class NameCubit extends Cubit<String> {
+  NameCubit(String state) : super(state);
+
+  void change(String name) => emit(name);
+}
+
+class NameContainer extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (_) => NameCubit("Kako Botasso"),
+      child: NameView(),
+    );
+  }
+}
+
+class NameView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController _nameController = TextEditingController();
+    _nameController.text = context.watch<NameCubit>().state;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Change name"),),
+      body: Column(
+        children: [
+          TextField(
+            controller: _nameController,
+            decoration: InputDecoration(labelText: "Desired name"),
+            style: TextStyle(
+              fontSize: 24.0,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top:8.0),
+            child: SizedBox(
+              width: double.maxFinite,
+              child: ElevatedButton(
+                child: Text("Change"),
+                onPressed: () {
+                  final name = _nameController.text;
+                  context.read<NameCubit>().change(name);
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
